f30af8676072dc7bf1602fe060822cf6589539e9
Calebe Lourenço Soares <calebelourenco@gmail.com>
2023-05-01 21:01:21 -0300

    Atualizando aula11

--- aula11/ex011.js
+++ aula11/ex011.js
@@ -1,9 +1,10 @@
-var idade = 19
+var idade = 66
+console.log(`Você tem ${idade} anos de idade.`)
 
 if(idade < 16){
     console.log('Não Vota')
-}else if(idade < 18){
+}else if(idade < 18 || idade > 65){
     console.log('Voto Opcional')
 }else {
-    console.log('Voto obrigatório')
+    console.log('Voto Obrigatório')
 }
