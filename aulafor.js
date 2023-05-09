23574ca4a5fa9d73692771f38c10c7cbc22ea21a
Calebe Lourenço Soares <calebelourenco@gmail.com>
2023-05-09 17:48:53 -0300

    aula de repetição

---
+++ aulafor.js
@@ -0,0 +1,13 @@
+/*
+var c = 1
+while (c <= 15){
+    console.log(c)
+    c++
+}
+*/
+//----------------------
+console.log("Vai começar...")
+for (c=1;c <= 10; c++){
+    console.log(c)
+}
+console.log("FIM!")
